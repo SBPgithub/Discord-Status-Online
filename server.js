f5805fc553a96e3164a7f2c7ebc2995804dcbd34
Glitch (discord-status-online) <none>
2020-06-21 21:41:44 +0000

    🌄🚲 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,46 @@
+// Tip: Add a 2 slashes infront of a code if you want ot disable it.
+
+// server.js
+// where your node app starts
+
+// init project
+const express = require("express");
+const fs = require("fs");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var Client = require("uptime-robot");
+const app = express();
+
+
+// Twitch Streaming Status
+client.on("ready", () => {
+console.log(`Logged in as ${client.user.tag}!`);
+
+client.user.setActivity(`jews llol`, {
+type: "STREAMING",
+url: "https://twitch.tv/YVNGPEDOtv6000"})
+    .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'none'}`))
+    .catch(console.error);
+});
+
+client.login(process.env.DISCORD_TOKEN)
+//Put 2 slashes infront of "client.login" to disable Twitch Streaming Status
+
+client.on('ready', () => {
+console.log("Logged in successfully!")});
+console.log ("Open source for the world to use.")
+console.log ("Made by YVNGPEDO6000")
+console.log ("Copyright © 2020 YVNGPEDO6000, a division Yvng Pedo, LLC.")
+console.log ("WARNING: Not accountable for account termination by official Discord Inc. moderators for breaking ToS. Do this at your own risk.")
+
+
+// this is the code for the guides
+app.use(require('./guides'));
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
